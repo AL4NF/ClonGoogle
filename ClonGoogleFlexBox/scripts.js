6b24f74473c787ede9d9b0e6f7242e8b4eb4ee08
Alan Fiscal <alanfiscalcampos@gmail.com>
2018-10-15 23:49:12 -0500

    Se añadio FlexBox

--- ClonGoogleFlexBox/scripts.js
+++ ClonGoogleFlexBox/scripts.js
@@ -1,9 +1,14 @@
 
+// Boton Buscar en Google
 const searchButton = document.querySelector(".buttonSerch");
 const searchInput = document.getElementById('form');
-
+// Boton Voy a Tener Suerte
+const searchButtonLuck = document.getElementById("buttonSerchLuck");
+// Boton Buscar en Google
 searchButton.addEventListener('click', search);
 searchInput.addEventListener('keypress', searchEnter);
+// Boton Voy a Tener Suerte
+searchButtonLuck.addEventListener('click', serchLuck);
 
 function search() {
   event.preventDefault();
@@ -19,4 +24,12 @@ function searchEnter() {
     let url = 'https://www.google.com/search?q=' + inputValue;
     window.location.assign(url);
   }
+}
+function serchLuck()
+{
+  event.preventDefault();
+  let inputValue = searchInput.value;
+  let url = 'http://www.google.com/search?btnI=1&q=' + inputValue;
+  window.location.assign(url);
+
 }
